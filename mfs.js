b2f5e354e0edd5c9835501476511844d49d3b15d
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-12-18 17:48:11 +0600

    here we have done how to move a html page using window.location.href

--- mfs.js
+++ mfs.js
@@ -7,4 +7,10 @@ document
     const phoneNumber = document.getElementById("phone-number").value;
     const pinNumber = document.getElementById("pin-number").value;
     console.log(phoneNumber, pinNumber);
+    if (phoneNumber === "5" && pinNumber === "1234") {
+      console.log("you are logged in");
+      window.location.href = "./home.html";
+    } else {
+      alert("invalid username or password");
+    }
   });
